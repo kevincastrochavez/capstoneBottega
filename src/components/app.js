60e88c56f9a946ece117e2f6687558bc56e176a1
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-27 20:09:03 -0500

    set up some routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout Page</h1>
+          </Route>
+
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
+
+export default App;
